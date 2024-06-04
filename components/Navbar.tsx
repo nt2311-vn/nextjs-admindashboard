fb7d0a6e371f48456884f84b23da3cf0eb39bf88
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-04 22:59:53 +0700

    add avatar components and implement on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/logo.png";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
 const Navbar = () => {
   return (
     <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
-      Navbar
+      <Link href="/">
+        <Image src={logo} alt="Diag" width={40} />
+      </Link>
+      <Avatar>
+        <AvatarImage src="https://github.com/nt2311-vn.png" alt="@nt2311-vn" />
+      </Avatar>
     </div>
   );
 };
