0c7e5152c092c6d6c81b8f09146fc030906be430
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-08 22:47:05 +0700

    adjust layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,8 @@ export default function RootLayout({
           <div className="hidden md:block h-[100vh] w-[300px]">
             <Sidebar />
           </div>
-          <div className="p-5 w-full md:max-w-[1140px]"></div>
+          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
         </div>
-        {children}
       </body>
     </html>
   );
