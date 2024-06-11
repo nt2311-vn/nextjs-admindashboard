0c29852ab5a722b403d70168af1f02368fc2ec7d
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-11 22:10:11 +0700

    use shadcnui table into post table

---
+++ components/posts/PostsTable.tsx
@@ -0,0 +1,37 @@
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableCaption,
+} from "@/components/ui/table";
+import Link from "next/link";
+import posts from "@/data/posts";
+import { Post } from "@/types/posts";
+
+interface PostsTableProps {
+  limit?: number;
+  title?: string;
+}
+
+const PostsTable = ({ limit, title }: PostsTableProps) => {
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>A list of recent posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden md:table-cell">Date</TableHead>
+          </TableRow>
+        </TableHeader>
+      </Table>
+    </div>
+  );
+};
+
+export default PostsTable;
