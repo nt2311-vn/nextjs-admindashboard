db67abc9e9a5a3c0e7c565b57512ae0b68a02cd7
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-14 00:29:47 +0700

    page tsx of edit post page

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -1,5 +1,33 @@
+"use client";
+import BackButton from "@/components/BackButton";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import posts from "@/data/posts";
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  body: z.string().min(1, { message: "Body is required" }),
+  author: z.string().min(1, { message: "Author is required" }),
+  date: z.string().min(1, { message: "Date is required" }),
+});
+
 const PostEditPage = () => {
-  return <div>Edit</div>;
+  return (
+    <>
+      <BackButton text="Back To Posts" link="/posts" />
+    </>
+  );
 };
 
 export default PostEditPage;
