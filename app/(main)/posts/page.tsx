2bd03ff2df90d6aa568b6c461bc1da08dc4d3f24
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-14 00:18:44 +0700

    implement pagination in page

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,9 +1,13 @@
 import PostsTable from "@/components/posts/PostsTable";
+import BackButton from "@/components/BackButton";
+import PostPagination from "@/components/posts/PostsPagination";
 
 const PostPage = () => {
   return (
     <>
+      <BackButton text="Go back" link="/" />
       <PostsTable />
+      <PostPagination />
     </>
   );
 };
