89514bd3b62e46a3f24fecef4b1a5ecbaea83716
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-15 22:13:43 +0700

    add zod form layout and validate data

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -22,10 +22,36 @@ const formSchema = z.object({
   date: z.string().min(1, { message: "Date is required" }),
 });
 
-const PostEditPage = () => {
+interface PostEditEditPageProps {
+  params: { id: string };
+}
+
+const PostEditPage = ({ params }: PostEditEditPageProps) => {
+  const post = posts.find((post) => post.id === params.id);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: post?.title || "",
+      body: post?.body || "",
+      author: post?.author || "",
+      date: post?.date || "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
     <>
       <BackButton text="Back To Posts" link="/posts" />
+      <h3 className="text-2xl mb-4">Edit Post</h3>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-8"
+        ></form>
+      </Form>
     </>
   );
 };
