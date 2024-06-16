b4987ddf98ed384da697cb14dbb737102fec5276
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-16 10:18:35 +0700

    implement themetoggle in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import ThemeToggler from "@/components/ThemeToggler";
 
 const Navbar = () => {
   return (
@@ -17,27 +18,30 @@ const Navbar = () => {
       <Link href="/">
         <Image src={logo} alt="Diag" width={40} />
       </Link>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="focus:outline-none">
-          <Avatar>
-            <AvatarImage
-              src="https://github.com/nt2311-vn.png"
-              alt="@nt2311-vn"
-            />
-            <AvatarFallback className="text-black">NT</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/auth">Logout</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="flex items-center">
+        <ThemeToggler />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarImage
+                src="https://github.com/nt2311-vn.png"
+                alt="@nt2311-vn"
+              />
+              <AvatarFallback className="text-black">NT</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
